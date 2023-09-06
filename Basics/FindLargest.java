0bc4cf57dc66d316bcb019eebc66bc8c23d3d200
suresh170411 <suresh.20fk@gmail.com>
2023-09-06 20:17:18 +0530

    Max of 3 numbers

---
+++ Basics/FindLargest.java
@@ -0,0 +1,29 @@
+package DSA_from_basics.Basics;
+
+import java.util.Scanner;
+
+public class FindLargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter 3 numbers");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+
+        findLargest(num1, num2, num3);
+        sc.close();
+    }
+
+    public static void findLargest(int num1, int num2, int num3){
+        int max = num1;
+
+        if (num2 > max){
+            max = num2;
+        }else if (num3 > max){
+            max = num3;
+        }
+
+        System.out.println("Max number in " + num1 +","+ num2 +","+ num3 +" is : " + max);
+    }
+}
