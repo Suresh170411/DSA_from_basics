ba5c24a7b5c8335e711ea263129a5c6f160560f2
Suresh170411 <suresh.20fk@gmail.com>
2023-09-07 15:18:39 +0530

    nested for loop

---
+++ Basics/ForLoop.java
@@ -0,0 +1,13 @@
+package DSA_from_basics.Basics;
+
+public class ForLoop {
+    
+    public static void main(String[] args) {
+        for (int family = 1; family <=4; family++){
+            for (int golgoppa = 1; golgoppa <= 10; golgoppa++){
+                System.out.println(family + " is eating " + golgoppa + " golguppa");
+            }
+        }  
+    }
+
+}
