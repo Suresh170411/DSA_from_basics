4aea5db4d97aaa1458389ce9a75e5e8b45f64c37
Suresh170411 <suresh.20fk@gmail.com>
2023-09-07 16:12:44 +0530

    For loops demo

--- Basics/ForLoop.java
+++ Basics/ForLoop.java
@@ -3,11 +3,28 @@ package DSA_from_basics.Basics;
 public class ForLoop {
     
     public static void main(String[] args) {
-        for (int family = 1; family <=4; family++){
-            for (int golgoppa = 1; golgoppa <= 10; golgoppa++){
-                System.out.println(family + " is eating " + golgoppa + " golguppa");
+        golguppaForFamily(10);    
+
+        printOneToFive(5);
+    }
+
+    public static void golguppaForFamily(int golgoppa){
+        for (int family=1; family<=4; family++){
+            for (int i=1; i<=golgoppa; i++){
+                System.out.println("Golgoppa : " + i + " to Family member : " + family);
             }
-        }  
+        }
+        
+    }
+
+    public static void printOneToFive(int times){
+        
+        for (int j=1; j<=times; j++){
+            for (int i=1; i<=5; i++){
+                System.out.println(i);
+            }
+            System.out.println();
+        }
     }
 
 }
