56078864884fb67b6ee1fbc59984e594fda884f7
Suresh170411 <suresh.20fk@gmail.com>
2023-09-07 17:14:31 +0530

    Pattern printing for loop

---
+++ Basics/PatterPrinting.java
@@ -0,0 +1,42 @@
+package DSA_from_basics.Basics;
+
+public class PatterPrinting {
+    public static void main(String[] args) {
+        // printTriangle1(2);
+
+        // System.out.println("\n");
+
+        // triangle2(5);
+        printInNums();
+    }
+
+    public static void printTriangle1(int n){
+        
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void triangle2(int n){
+
+        for (int i=1; i<=n; i++){
+            for (int j=n; j>=i; j--){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printInNums(){
+        
+        for (int i=5; i>=1; i--){
+            for (int j=1; j<=i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
