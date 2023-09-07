6b4f48b9933b6e6051fa99139c41e54c1bdce4fd
Suresh170411 <suresh.20fk@gmail.com>
2023-09-07 16:25:45 +0530

    For loop father son duo

--- Basics/ForLoop.java
+++ Basics/ForLoop.java
@@ -3,9 +3,11 @@ package DSA_from_basics.Basics;
 public class ForLoop {
     
     public static void main(String[] args) {
-        golguppaForFamily(10);    
+        // golguppaForFamily(10);    
 
-        printOneToFive(5);
+        // printOneToFive(5);
+
+        fatherSonDuo();
     }
 
     public static void golguppaForFamily(int golgoppa){
@@ -22,6 +24,18 @@ public class ForLoop {
         for (int j=1; j<=times; j++){
             for (int i=1; i<=5; i++){
                 System.out.println(i);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void fatherSonDuo(){
+        
+        // father said son to plant 10 seeds each on 5 of the farms
+
+        for (int j=1; j<=5; j++){
+            for (int i=1; i<=10; i++){
+                System.out.println(i); //plant 10 seeds
             }
             System.out.println();
         }
