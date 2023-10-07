01487ad48da4b243d0aa32c1e939d0443ab8d6eb
suresh170411 <suresh.20fk@gmail.com>
2023-10-07 10:46:20 +0530

    Move Zeroes - I

---
+++ Array/MoveZeros1.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class MoveZeros1{
+    public static void main(String[] args) {
+        int arr [] = {0,1,0,3,12};
+
+        moveZeroesBruteForce(arr);
+    }
+
+    public static void moveZeroesBruteForce(int arr []){
+        int n = arr.length;
+
+        int newArr [] = new int [n];
+        int index = 0;
+        for (int i=0; i<n; i++){
+            if (arr[i] != 0){
+                newArr[index++] = arr[i];
+            }
+        }
+
+        System.out.println(Arrays.toString(newArr));
+    }
+}
