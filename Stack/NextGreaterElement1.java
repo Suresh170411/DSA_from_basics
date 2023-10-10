9c355f8a61adb445cf96524a3e5c1008aa265294
suresh170411 <suresh.20fk@gmail.com>
2023-10-10 14:16:09 +0530

    Next; Greater Element

--- Stack/NextGreaterElement1.java
+++ Stack/NextGreaterElement1.java
@@ -8,6 +8,7 @@ public class NextGreaterElement1 {
         int arr [] = {1,3,89,10};
 
         nextGreaterElement(arr);
+        NGE(arr);
     }
 
     public static void nextGreaterElement(int arr []){
@@ -24,6 +25,23 @@ public class NextGreaterElement1 {
             }
             res[i] = !st.isEmpty() ? arr[st.peek()] : -1;
             st.push(i);
+        }
+        System.out.println(Arrays.toString(res));
+    }
+
+    public static void NGE(int arr[]){
+        Stack<Integer> stack = new Stack<>();
+        int[] res = new int[arr.length];
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            
+            res[i] = stack.isEmpty() ? -1 : stack.peek();
+
+            stack.push(arr[i]);
         }
         System.out.println(Arrays.toString(res));
     }
