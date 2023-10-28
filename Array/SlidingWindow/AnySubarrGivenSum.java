b52e7c7af97e7b5e3e0875fd4f6292715056e237
suresh170411 <suresh.20fk@gmail.com>
2023-10-28 16:19:33 +0530

    Find any subarray with the given sum

--- Array/SlidingWindow/AnySubarrGivenSum.java
+++ Array/SlidingWindow/AnySubarrGivenSum.java
@@ -4,9 +4,11 @@ public class AnySubarrGivenSum {
     public static void main(String[] args) {
         int arr [] = {1,4,20,3,10,5};
         int sum = 33;
-        boolean res = slidingWindowTechnique(arr, sum);
 
+        boolean res = slidingWindowTechnique(arr, sum);
         System.out.println(res);
+
+        System.out.println(bruteForce(arr, sum));
     }
 
     public static boolean slidingWindowTechnique(int arr[], int sum){
@@ -25,6 +27,22 @@ public class AnySubarrGivenSum {
             }
 
             windowSum = windowSum - arr[i];
+        }
+        return false;
+    }
+
+    public static boolean bruteForce(int arr [], int sum){
+        
+        int n = arr.length;
+
+        for (int i=0; i<n; i++){
+            for (int j=i; j<n; j++){
+                int curr_sum = 0;
+                for (int k=i; k<=j; k++){
+                    curr_sum+= arr[k];
+                }
+                if (curr_sum == sum) return true;
+            }
         }
         return false;
     }
