050d7e8b642ff3fd63755365d0b6b7a8df1e2a73
suresh170411 <suresh.20fk@gmail.com>
2023-10-31 22:57:22 +0530

    Print 1 to N using recursion

---
+++ Recursion/PrintNums.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class PrintNums {
+    public static void main(String[] args) {
+        int n = 5;
+        printNums(n);
+    }
+
+    public static void printNums(int n){
+        if (n==0) return;
+
+        printNums(n-1);
+        System.out.println(n);
+    }
+}
