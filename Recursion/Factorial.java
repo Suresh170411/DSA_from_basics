d349e0f76a5147283a5848f13ae3205e7d00e6aa
suresh170411 <suresh.20fk@gmail.com>
2023-11-02 19:02:19 +0530

    Factorial of number

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -1,9 +1,11 @@
-package Recursion;
+// package Recursion;
 
 public class Factorial {
     public static void main(String[] args) {
-        int n = 3;
+        int n = 5;
         brureForece(n);
+        int res = factorialUsingRecursion(n);
+        System.out.println(res);
     }
 
     public static void brureForece(int n){
@@ -15,7 +17,8 @@ public class Factorial {
         System.out.println(res);
     }
 
-    public static void factorialUsingRecursion(int n){
-        
+    public static int factorialUsingRecursion(int n){
+        if (n==0 || n==1) return n;
+        else return n*factorialUsingRecursion(n-1);
     }
 }
