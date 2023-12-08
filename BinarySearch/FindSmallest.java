962d301acc34a276c82d5255a571b65fb9640e5d
suresh170411 <suresh.20fk@gmail.com>
2023-12-08 20:19:16 +0530

    Find the smallest element from sorted rotated array

---
+++ BinarySearch/FindSmallest.java
@@ -0,0 +1,29 @@
+public class FindSmallest {
+    public static void main(String[] args) {
+        int arr [] = {4,5,6,7,8,9,10,1,2};
+        int n = arr.length;
+
+        int res = findSmallestFromSortedRotetedArr(arr, n);
+        System.out.println(arr[res]);
+    }
+
+    public static int findSmallestFromSortedRotetedArr(int arr [], int n){
+        int low = 0;
+        int high = n-1;
+
+        while (low <= high) {
+            int mid = low+(high-low)/2;
+
+            if (arr[mid-1] > arr[mid]){
+                return mid;
+            }else if (arr[mid] > arr[mid+1]){
+                return mid+1;
+            }else if (arr[low] <= arr[mid]){
+                low = mid+1;
+            }else {
+                high = low-1;
+            }
+        }
+        return -1;
+    }
+}
